27a149c1cd46db5b31c95d2fba995e1e2a810cda
YonatanQuintero <yonax73@gmail.com>
2024-02-15 12:28:58 -0500

    Redirect dashboard to home

--- src/dashboard/components/routes/dashboard-routes.component.tsx
+++ src/dashboard/components/routes/dashboard-routes.component.tsx
@@ -1,14 +1,19 @@
 import DashboardLayout from "../layout/dashboard-layout.component"
 import DashboardIndex from "../../home/components/index/dashboard-index.component"
 import UserIndex from "../../users/components/index/user-index.component"
+import { Navigate } from "react-router-dom"
 
 const DashboardRoutes = [
+  {
+    path: "/dashboard",
+    element: <Navigate to="/dashboard/home" replace />,
+  },
   {
     path: "/dashboard",
     element: <DashboardLayout />,
     children: [
       {
-        index: true,
+        path: "/dashboard/home",
         element: <DashboardIndex />,
       },
       {
