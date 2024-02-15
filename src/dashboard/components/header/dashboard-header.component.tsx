da6333ce8694b8d67e2fa473149b5ec73654ca09
YonatanQuintero <yonax73@gmail.com>
2024-02-15 14:31:38 -0500

    Implement dashboard layout

--- src/dashboard/components/header/dashboard-header.component.tsx
+++ src/dashboard/components/header/dashboard-header.component.tsx
@@ -1,42 +1,27 @@
-import { useState } from "react"
 import { Link } from "react-router-dom"
-import { Nav, NavItem, NavLink, Container, Offcanvas, OffcanvasBody } from "reactstrap"
+import { Container, Row, Col } from "reactstrap"
 
 export default function DashboardHeader() {
-  const [offcanvasOpen, setOffcanvasOpen] = useState(false)
-
-  const toggleOffcanvas = () => setOffcanvasOpen(!offcanvasOpen)
-
   return (
-    <header className="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
+    <header className="p-3 text-bg-dark">
       <Container fluid>
-        <Nav col-md="3" col-lg="2" className="me-0 px-3 fs-6 text-white">
-          <Link
-            to="/dashboard"
-            className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
-          >
-            <img
-              src="/icons/logo-white-520.png"
-              alt="Elox Pro Logo"
-              width="40"
-              height="40"
-              className="bi pe-none me-2"
-            />
-            <span className="fs-4">Elox Pro</span>
-          </Link>
-        </Nav>
-
-        <Nav className="flex-row d-md-none">
-          <NavItem>
-            <NavLink className="px-3 text-white" type="button" onClick={toggleOffcanvas}>
-              <i className="bi bi-list"></i>
-            </NavLink>
-          </NavItem>
-        </Nav>
-
-        <Offcanvas isOpen={offcanvasOpen} onToggle={toggleOffcanvas}>
-          <OffcanvasBody>Offcanvas content here</OffcanvasBody>
-        </Offcanvas>
+        <Row className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <Col xs="12" lg="auto">
+            <Link
+              to="/dashboard/home"
+              className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
+            >
+              <img
+                src="/icons/logo-white-520.png"
+                alt="Elox Pro Logo"
+                width="40"
+                height="40"
+                className="bi pe-none me-2"
+              />
+              Elox Pro
+            </Link>
+          </Col>
+        </Row>
       </Container>
     </header>
   )
--- src/dashboard/components/layout/dashboard-layout.component.tsx
+++ src/dashboard/components/layout/dashboard-layout.component.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom"
+import DashboardSidebar from "../sidebar/dashboard-sidebar.component"
 import DashboardHeader from "../header/dashboard-header.component"
 
 export default function DashboardLayout() {
   return (
     <>
       <DashboardHeader />
-      <Outlet />
+      <DashboardSidebar>
+        <Outlet />
+      </DashboardSidebar>
     </>
   )
 }
