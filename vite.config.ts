4077e5766d07d25e91ed67bdb3abe990a158f5c6
YonatanQuintero <yonax73@gmail.com>
2024-02-19 19:41:39 -0500

    Install ssl in the ngix server

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // I solved the issue by adding this line
+    // Uncaught (in promise) DOMException: Failed to construct 'WebSocket':
+    // The URL 'wss://localhost:undefined/' is invalid
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+      port: 3000,
+    },
+  },
+
 })
