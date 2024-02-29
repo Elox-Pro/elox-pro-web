21fe70bde68fe121e77d43afd12ba06d77ff5408
YonatanQuintero <yonax73@gmail.com>
2024-02-29 14:02:46 -0500

    Implement cover page

--- src/cover/components/index/cover-index.component.tsx
+++ src/cover/components/index/cover-index.component.tsx
@@ -6,7 +6,7 @@ import CoverFooter from "../footer/cover-footer.component"
 
 export default function CoverIndex() {
   return (
-    <div className="d-flex h-100 text-center bd-masthead">
+    <div className="cover-index d-flex h-100 text-center bd-masthead">
       <Container fluid className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
         <CoverHeader />
         <CoverMain />
