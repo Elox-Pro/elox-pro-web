5f12b3be629ccd076e871707b1c159dbf3d10c9c
YonatanQuintero <yonax73@gmail.com>
2024-03-01 17:09:03 -0500

    Refactor dashboard layout

--- src/dashboard/components/header/dashboard-header.component.tsx
+++ src/dashboard/components/header/dashboard-header.component.tsx
@@ -1,24 +1,9 @@
-import { Row, Col } from "reactstrap"
-import BrandLink from "../../../common/components/brand-link/brand-link.component"
-import DropdownProfile from "../dropdown-profile/dropdown-profile.component"
-import "./dashboard-header.style.scss"
+import DashboardNavbar from "../navbar/dashboard-navbar.component"
 
 export default function DashboardHeader() {
   return (
-    <header className="dashboard-header sticky-top text-bg-dark">
-      <Row className="g-0 align-items-center">
-        <Col lg="2" md="3">
-          <BrandLink to="/dashboard/home" isText={true} />
-        </Col>
-        <Col lg="10" md="9" className="text-end">
-          <ul className="navbar-nav flex-row justify-content-end">
-            <li className="nav-item">Another item</li>
-            <li className="nav-item">
-              <DropdownProfile />
-            </li>
-          </ul>
-        </Col>
-      </Row>
+    <header className="sticky-top">
+      <DashboardNavbar />
     </header>
   )
 }
---
+++ src/dashboard/components/navbar/dashboard-navbar.component.tsx
@@ -0,0 +1,41 @@
+import Container from "react-bootstrap/Container"
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar"
+import NavDropdown from "react-bootstrap/NavDropdown"
+import DashboardNavbarBrand from "../navbar-brand/dashboard-navbar-brand.component"
+import useActiveUser from "../../../auth/hooks/active-user.hook"
+import { Link } from "react-router-dom"
+import "./dashboard-navbar.style.scss"
+export default function DashboardNavbar() {
+  const activeUser = useActiveUser()
+
+  return (
+    <Navbar expand="lg" bg="dark" data-bs-theme="dark" className="dashboard-navbar">
+      <Container fluid>
+        <DashboardNavbarBrand />
+        <Nav className="me-auto">
+          <Nav.Link href="#">Toogle</Nav.Link>
+        </Nav>
+        <Nav className="ms-auto">
+          <NavDropdown
+            title={
+              <span>
+                {activeUser?.sub}
+                <i className="ms-2 bi bi-person-circle"></i>
+              </span>
+            }
+            id="dropdown-profile"
+          >
+            <Link to="dashboard/user/profile" className="dropdown-item">
+              Profile
+            </Link>
+            <NavDropdown.Divider />
+            <Link to="auth/logout" className="dropdown-item">
+              Logout
+            </Link>
+          </NavDropdown>
+        </Nav>
+      </Container>
+    </Navbar>
+  )
+}
